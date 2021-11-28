152887e8dd7ee50b11b758ddd18b2f24ecf0d45e
Maxime Eychenne <maxime.eychenne@gmail.com>
2021-11-29 00:49:24 +0100

    fix: order team by name in match form

--- src/Form/FootballMatchType.php
+++ src/Form/FootballMatchType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\FootballMatch;
 use App\Entity\Team;
 use App\Entity\Tournament;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -21,11 +22,18 @@ class FootballMatchType extends AbstractType
             ->add('hostingTeamScore', NumberType::class)
             ->add('receivingTeamScore', NumberType::class)
             ->add('hostingTeam', EntityType::class, [
-                'class' => Team::class
+                'class' => Team::class,
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder('t')
+                        ->orderBy('t.name', 'ASC');
+                }
             ])
             ->add('receivingTeam', EntityType::class, [
-                'class' => Team::class
-            ])
+                'class' => Team::class,
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder('t')
+                        ->orderBy('t.name', 'ASC');
+                }])
             ->add('tournament', EntityType::class, [
                 'class' => Tournament::class
             ])
