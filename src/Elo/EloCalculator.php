56bc0d819e1a96cbc51cab5630081047fc958f20
Maxime Eychenne <maxime.eychenne@gmail.com>
2021-11-29 01:05:41 +0100

    feat: adjust elo coefficient

--- src/Elo/EloCalculator.php
+++ src/Elo/EloCalculator.php
@@ -10,12 +10,23 @@ class EloCalculator
 
     public function getEloEvolution(int $rating, int $opponentRating, float $result): int
     {
+        $k = 80;
+        if ($rating > 1600 || $opponentRating > 1600) {
+            $k = 40;
+        }
+        if ($rating > 2000 || $opponentRating > 2000) {
+            $k = 20;
+        }
+        if ($rating > 2400 || $opponentRating > 2400) {
+            $k = 10;
+        }
+
         $eloDiff = $rating - $opponentRating;
         if ($eloDiff > 400) {
             $eloDiff = 400;
         }
         $winProbability = 1 / (1 + (10 ** (-$eloDiff / 400)));
 
-        return 80 * ($result - $winProbability);
+        return $k * ($result - $winProbability);
     }
 }
