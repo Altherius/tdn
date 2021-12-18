a3710d20898d5deccc670e46bde9451acf515704
Maxime Eychenne <maxime.eychenne@gmail.com>
2021-12-18 05:52:24 +0100

    fix: destroy third party js before turbo cache

--- assets/controllers/select2_controller.js
+++ assets/controllers/select2_controller.js
@@ -2,10 +2,23 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
     connect() {
-        $('.select2').select2({
+        $('select.select2').select2({
             theme: "bootstrap-5",
             selectionCssClass: "select2--small",
         });
+
+        document.addEventListener("turbo:before-cache", function() {
+            $("select.select2").each(function() {
+                $(this).select2('destroy');
+                console.log('select2 destroyed');
+            });
+
+            $('table.table').each(function() {
+                $(this).bootstrapTable('destroy');
+                console.log('table destroyed');
+            });
+        });
+
         $('table.table').bootstrapTable({
             search: true,
             locale: "fr-FR",
